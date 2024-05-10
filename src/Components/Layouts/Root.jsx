4629bf662175688c2cb85380f9a01fe002825176
Jubayer-K <jubayerkhan21@gmail.com>
2024-05-11 03:22:18 +0600

    added hero and headline section

--- src/Components/Layouts/Root.jsx
+++ src/Components/Layouts/Root.jsx
@@ -1,25 +1,29 @@
 import { Outlet } from "react-router-dom";
-import { ToastContainer,Flip } from "react-toastify";
+import { ToastContainer, Flip } from "react-toastify";
 import NavigationBar from "../Shared/Navbar/NavigationBar";
 import Footer from "../Shared/Footer/Footer";
+import Headline from "../Shared/Headline/Headline";
 const Root = () => {
   return (
-    <div className="font-raleway dark:bg-gray-800 dark:text-white">
-      <NavigationBar></NavigationBar>
-      <Outlet></Outlet>
-      <Footer></Footer>
-      <ToastContainer
-        transition={Flip}
-        position="bottom-center"
-        autoClose={3000}
-        hideProgressBar={true}
-        newestOnTop
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      ></ToastContainer >
-    </div>
+    <>
+      <Headline></Headline>
+      <div className="font-raleway dark:bg-gray-800 dark:text-white">
+        <NavigationBar></NavigationBar>
+        <Outlet></Outlet>
+        <Footer></Footer>
+        <ToastContainer
+          transition={Flip}
+          position="bottom-center"
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        ></ToastContainer>
+      </div>
+    </>
   );
 };
 
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,15 +1,17 @@
 import Banner from "../../Shared/Banner/Banner";
+import Hero from "../../Shared/Hero/Hero";
 import Newsletter from "../../Shared/Newsletter/Newsletter";
 import TopBloggers from "../../Shared/TopBloggers/TopBloggers";
 
 const Home = () => {
   return (
     <>
-      <div className=" mx-auto md:py-12 py-6">
+      <Hero></Hero>
+      <TopBloggers></TopBloggers>
+      <div className=" mx-auto max-w-screen-2xl md:py-12 py-6">
         <Banner></Banner>
-        <TopBloggers></TopBloggers>
-        <Newsletter></Newsletter>
       </div>
+      <Newsletter></Newsletter>
     </>
   );
 };
